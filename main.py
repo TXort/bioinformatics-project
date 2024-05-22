1fd7209ce952390030608b93d62a73729c382c31
TXort <46675337+TXort@users.noreply.github.com>
2024-05-22 09:21:53 +0200

    Add type notations

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 from typing import Dict
 
+import Bio
 from Bio import SeqIO
+import pandas as pd
+from Bio.Seq import Seq
+from scipy.spatial.distance import cosine
 
 
-
-def read_sequence(file_path: str, format: str) -> str:
+def read_sequence(file_path: str, format: str) -> Seq:
     for record in SeqIO.parse(file_path + "." + format, format):
         return record.seq
 
-seq = read_sequence("r", "fastq")
 
-print(len(seq))
-
-
-def get_distribution(seq, k):
-    k_mer = {}
+def get_distribution(seq: str, k: int) -> Dict[str, float]:
+    k_mer: Dict[str, int] = {}
 
     for i in range(len(seq) - k + 1):
         if seq[i:i + k] not in k_mer:
             k_mer[seq[i:i + k]] = 0
         k_mer[seq[i:i + k]] += 1
 
-    k_mer_distribution = {}
-    n = sum(k_mer.values())
+    k_mer_distribution: Dict[str, float] = {}
+
+    div: int = sum(k_mer.values())
+
     for x, y in k_mer.items():
-        k_mer_distribution[x] = y / n
+        k_mer_distribution[x] = y / div
 
     return k_mer_distribution
 
 
-seq2 = read_sequence("klebsiella_pneumoniae_reference-1", "fasta")
+seq1: Seq = read_sequence("r", "fastq")
+seq2: Seq = read_sequence("klebsiella_pneumoniae_reference-1", "fasta")
+
 
 seq2 = seq2.replace("N", "T")
 
-distr1 = get_distribution(seq, 3)
-distr2 = get_distribution(seq2, 3)
+distr1: Dict[str, float] = get_distribution(seq1, 3)
+distr2: Dict[str, float] = get_distribution(seq2, 3)
 
 print(set(distr2.keys()))
 
 print(len(distr2))
-import pandas as pd
-from scipy.spatial.distance import cosine
 
 line_s = pd.Series(distr1)
 query_s = pd.Series(distr2)
